f4fa5db1f02de81e0a4a9a2898bf305e3bee5e74
Ethan Welsh <ejw45@pitt.edu>
2015-09-14 22:58:02 -0400

    remove project files

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-__author__ = 'welshej'
+import numpy
+
+
+def read_board_from_file(file_name):
+    """
+    :param file_name:
+    :return: numpy character matrix
+    :return: direction string
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()

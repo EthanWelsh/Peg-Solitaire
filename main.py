9d74ffe16db887315eecf5160d7141426e32d51c
Ethan Welsh <ejw45@pitt.edu>
2015-09-15 01:30:27 -0400

    add check to see what moves are possible into a free spot

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import numpy as np
 
 
 class Board:
-    def __init__(self, board):
+    def __init__(self, board, directions):
         self.board = board
         self.size = board.shape[0]
+
+        if directions == 'all':
+            self.directions = ['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw']
+        elif directions == 'ortho:':
+            self.directions = ['n', 'e', 's', 'w']
+        elif directions == 'swne':
+            self.directions = ['n', 'ne', 'e', 's', 'sw', 'w']
+        else:
+            assert False
 
     def check_peg(self, start_position, direction):
         return self._get_spot(start_position, direction) == '*'
@@ -12,20 +21,37 @@ class Board:
     def check_free(self, start_position, direction):
         return self._get_spot(start_position, direction) == 'o'
 
+    def _move_direction(self, start_position, direction):
+        """
+        :param start_position: The coordinates that you'd like to start from
+        :param direction: The direction that you'd like to look in
+        :return: The adjusted index of (r, c) after moving in a certain direction
+        """
+        r, c = start_position
+
+        if 'n' in direction:
+            r -= 1 * direction.count('n')
+        if 'e' in direction:
+            c += 1 * direction.count('e')
+        if 's' in direction:
+            r += 1 * direction.count('s')
+        if 'w' in direction:
+            c -= 1 * direction.count('w')
+
+        return r, c
+
     def _get_spot(self, start_position, direction):
+        """
+        :param start_position: The coordinates that you'd like to start from
+        :param direction: The direction that you'd like to look in
+        :return: the character at that position in the board
+        """
         r, c = start_position
 
         if self._out_of_bounds(r, c):
             return '.'
 
-        if 'n' in direction:
-            r -= 1
-        if 'e' in direction:
-            c += 1
-        if 's' in direction:
-            r += 1
-        if 'w' in direction:
-            c -= 1
+        r, c = self._move_direction(start_position, direction)
 
         if self._out_of_bounds(r, c):
             return '.'
@@ -33,9 +59,16 @@ class Board:
         return self.board[r, c]
 
     def _out_of_bounds(self, r, c):
+        """
+        Checks to see if a given r, c is out of bounds
+        """
         return min(r, c) < 0 or max(r, c) > self.size
 
-    def get_blank_spots(self):
+    def _get_blank_spots(self):
+        """
+        Looks through the matrix and returns a list of empty spaces.
+        :returns a list of (r, c) coordinates where blank spots can be found on the board.
+        """
         positions = []
         for r in range(self.size):
             for c in range(self.size):
@@ -43,6 +76,20 @@ class Board:
                     positions.append((r, c))
 
         return positions
+
+    def _possible_jumps_into_empty(self, empty_coord):
+        """
+        Given the coordinate of an empty space, will return the locations of all the pegs that could jump into that cell
+        :param empty_coord: the coordinate of the empty spot that you'd like to check from
+        """
+        assert self.board[empty_coord] == 'o'
+        possible_jumps = []
+
+        for direction in self.directions:
+            if self.check_peg(empty_coord, direction) and self.check_peg(empty_coord, direction * 2):
+                possible_jumps.append(self._move_direction(empty_coord, direction * 2))
+
+        return possible_jumps
 
 
 def read_board_from_file(file_name):
@@ -55,13 +102,12 @@ def read_board_from_file(file_name):
         directions, _, *matrix_lines = peg_file.readlines()
 
     board_matrix = np.array([[spot for spot in line.strip().split(' ')] for line in matrix_lines])
-    return board_matrix, directions
+    return board_matrix, directions.strip()
 
 
 def main():
     matrix, directions = read_board_from_file('input_files/test.txt')
-    board = Board(matrix)
-    print(board.get_blank_spots())
+    board = Board(matrix, directions)
 
 
 if __name__ == '__main__':

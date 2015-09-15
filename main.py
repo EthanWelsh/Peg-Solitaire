8f53f50b1a05abd0e9afad3c67aff28808ff08fd
Ethan Welsh <ejw45@pitt.edu>
2015-09-14 23:18:22 -0400

    read board in from file

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import numpy
+import numpy as np
 
 
 def read_board_from_file(file_name):
     """
     :param file_name:
-    :return: numpy character matrix
-    :return: direction string
+    :return: board_matrix: numpy character matrix
+    :return: directions: string enumerating the directions travel is allowed
     """
-    pass
+    with open(file_name, 'r') as peg_file:
+        directions, _, *matrix_lines = peg_file.readlines()
+
+    board_matrix = np.array([[spot for spot in line.strip().split(' ')] for line in matrix_lines])
+    return board_matrix, directions
 
 
 def main():
-    pass
+    board, directions = read_board_from_file('input_files/sample00.txt')
+    print(board[0, 0])
 
 
 if __name__ == '__main__':

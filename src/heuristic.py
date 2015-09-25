87f4aca59bfef745e4eb47e8a1c8484ad2678c0d
Ethan Welsh <ejw45@pitt.edu>
2015-09-25 11:58:22 -0400

    loss state boards give a value of inf

--- src/heuristic.py
+++ src/heuristic.py
@@ -3,20 +3,35 @@ from src.board import Board
 
 def max_moves(board):
     pegs_on_board = board.peg_count()
-    pegs_with_move, _ = zip(board.get_possible_moves())
-    pegs_with_move = len(list(set(pegs_with_move)))
+    moves = [move for move in board.get_possible_moves()]
+
+    if len(moves) == 0:
+        return float('inf')
+
+    moves, _ = zip(*moves)
+    pegs_with_move = len(list(set(moves)))
     return pegs_on_board - 1 - (pegs_with_move / pegs_on_board)
 
 
 def min_moves(board):
     pegs_on_board = board.peg_count()
-    moves, _ = zip(board.get_possible_moves())
+    moves = [move for move in board.get_possible_moves()]
+
+    if len(moves) == 0:
+        return float('inf')
+
+    moves, _ = zip(*moves)
     moves = len(moves)
     return pegs_on_board - 1 - moves / (moves + 1)
 
 
 def max_movable_pegs(board):
     pegs_on_board = board.peg_count()
-    moves, _ = zip(board.get_possible_moves())
+    moves = [move for move in board.get_possible_moves()]
+
+    if len(moves) == 0:
+        return float('inf')
+
+    moves, _ = zip(*moves)
     moves = len(moves)
     return pegs_on_board - 1 - 1/(moves + 1)

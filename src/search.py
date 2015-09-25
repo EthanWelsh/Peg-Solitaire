a2a8335c4d68775b95415e37d1c1afd35c8d92b2
Ethan Welsh <ejw45@pitt.edu>
2015-09-25 12:26:57 -0400

    fix problems with IDA*

--- src/search.py
+++ src/search.py
@@ -1,13 +1,14 @@
 import sys
 
 import itertools
+import math
 
 from src.board import Board
 from src.priority_queue import PriorityQueue
 import src.heuristic as heuristics
 
-class DepthFirstSearch:
 
+class DepthFirstSearch:
     def __init__(self, start):
         self.start = start
 
@@ -40,7 +41,6 @@ class BreadthFirstSearch:
 
 
 class AStar:
-
     def __init__(self, start, heuristic):
         self.start = start
         self.heuristic = heuristic
@@ -58,7 +58,6 @@ class AStar:
 
 
 class IterativeDeepeningAStar:
-
     def __init__(self, start, heuristic):
         self.start = start
         self.heuristic = heuristic
@@ -66,25 +65,30 @@ class IterativeDeepeningAStar:
     def search(self):
 
         def depth_limited_astar(depth, state, path, heuristic):
-            if len(path) + heuristic(state) > depth:
-                return
-
+            score = len(path) + heuristic(state)
+            if score > depth:
+                return None, score
             if state.is_goal():
-                return path
+                return path, score
 
+            min = float('inf')
             for move, board in state.successors():
-                z = depth_limited_astar(depth, board, path + [move], heuristic)
+                z, s = depth_limited_astar(depth, board, path + [move], heuristic)
+                if s < min:
+                    min = s
                 if z is not None:
-                    return z
+                    return z, s
+            return None, min
 
-        for depth in itertools.count():
-            x = depth_limited_astar(depth, self.start, [], self.heuristic)
+        x = None
+        depth = 0
+        while x is None:
+            x, depth = depth_limited_astar(depth, self.start, [], self.heuristic)
             if x is not None:
                 return x
 
 
 def main():
-
     start_board = Board.board_from_file(sys.argv[1])
     method = sys.argv[2]
     heuristic = sys.argv[3]
@@ -110,6 +114,7 @@ def main():
         print(itd_astar.search())
     else:
         print("You must choose a valid method")
+
 
 if __name__ == '__main__':
     main()
